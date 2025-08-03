3d23b17b8836da33fa6ed9e3ce0ce03092c1ca96
Alejandro Bentos <alius76@gmail.com>
2025-08-03 13:17:28 +0200

    Screens faltantes

---
+++ composeApp/src/commonMain/kotlin/com/alius/gmrstock/presentation/screens/ProcessScreenContent.kt
@@ -0,0 +1,9 @@
+package com.alius.gmrstock.presentation.screens
+
+import androidx.compose.runtime.Composable
+import com.alius.gmrstock.domain.model.User
+
+@Composable
+fun ProcessScreenContent(user: User) {
+    // usa user aquí dentro
+}
